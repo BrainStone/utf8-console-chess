2590d65ecd903e0b25ff4542c6cc7a0c58fa7891
BrainStone <yannick@tedworld.de>
2023-05-03 23:05:02 +0200

    Removed failed dummy tests

--- test/test.cpp
+++ test/test.cpp
@@ -4,15 +4,3 @@
 TEST(Dummy_1, Success) {
     EXPECT_LT(1, 2);
 }
-
-TEST(Dummy_1, Failure) {
-    EXPECT_LT(2, 1);
-}
-
-TEST(Dummy_2, Success) {
-    EXPECT_GT(2, 1);
-}
-
-TEST(Dummy_2, Failure) {
-    EXPECT_GT(1, 2);
-}
